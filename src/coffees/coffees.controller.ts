407ff234e3450fc636be7fe8715a66fd476b8d30
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-07-20 11:55:12 +0200

    complete 02 chapter

---
+++ src/coffees/coffees.controller.ts
@@ -0,0 +1,59 @@
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
+} from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
+import { CreateCoffeeDto } from './dto/create-coffee.dto';
+import { UpdateCoffeeDto } from './dto/update-coffee.dto';
+
+@Controller('coffees')
+export class CoffeesController {
+    constructor(private readonly coffeesService: CoffeesService) {}
+
+    @Get()
+    findAll(@Query() paginationQueryParams) {
+        // const { limit, offset } = paginationQueryParams;
+        return this.coffeesService.findAll();
+    }
+    // @Get()
+    // findAll(@Res() response) {
+    //     response.status(202).send('This action returns all coffees');
+    // }
+
+    @Get(':id')
+    findOne(@Param('id') id: number) {
+        console.log(typeof id);
+        return this.coffeesService.findOne(id);
+    }
+    // @Get(':id')
+    // findOne(@Param() param) {  ===> this is how u get an object with all params
+    //     return `This action returns #${param.id} coffee`
+    // }
+
+    @Post()
+    create(@Body() createCoffeeDto: CreateCoffeeDto) {
+        console.log(createCoffeeDto instanceof CreateCoffeeDto);
+        return this.coffeesService.create(createCoffeeDto);
+    }
+    // @Post()
+    // @HttpCode(HttpStatus.CREATED) ===> this is a 201 status code
+    // create(@Body('name') name) {
+    //     return name
+    // }
+
+    @Patch(':id')
+    update(@Param('id') id: number, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+        return this.coffeesService.update(id, updateCoffeeDto);
+    }
+
+    @Delete(':id')
+    remove(@Param('id') id: number) {
+        return this.coffeesService.remove(id);
+    }
+}
