ed58ec4c657a5d2f697fcbad98f39cde0dec88e8
Radoslaw Czerniawski <radoslaw.czerniawski@bshg.com>
2022-07-30 19:26:41 +0200

    async factory

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Injectable, Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { MongooseModule } from '@nestjs/mongoose';
@@ -7,10 +7,20 @@ import { EventSchema } from 'src/events/entities/event.entity';
 import { COFFEE_BRANDS } from './coffees.constants';
 
 // class MockCoffeesService {}
+class ConfigService {}
+class DevelopmentConfigService {}
+class ProductionConfigService {}
+
+@Injectable()
+export class CoffeeBrandsFactory {
+    create() {
+        return ['buddy brew', 'nescafe'];
+    }
+}
 
 @Module({
     imports: [
-    MongooseModule.forFeature([
+        MongooseModule.forFeature([
             {
                 name: Coffee.name,
                 schema: CoffeeSchema,
@@ -24,8 +34,29 @@ import { COFFEE_BRANDS } from './coffees.constants';
     controllers: [CoffeesController],
     providers: [
         CoffeesService,
+        // CoffeeBrandsFactory,
+        {
+            provide: COFFEE_BRANDS,
+            // useFactory: (brandsFactory: CoffeeBrandsFactory) => brandsFactory.create();
+            useFactory: async (): Promise<string[]> => {
+                // const coffeeBrands = await connection to the database
+                const coffeeBrands = await Promise.resolve([
+                    'buddy brew',
+                    'nescafe',
+                ]);
+
+                return coffeeBrands;
+            },
+            // inject: [CoffeeBrandsFactory],
+        },
+        {
+            provide: ConfigService,
+            useClass:
+                process.env.NODE_ENV === 'development'
+                    ? DevelopmentConfigService
+                    : ProductionConfigService,
+        },
         // { provide: CoffeesService, useValue: new MockCoffeesService() },
-        {provide: COFFEE_BRANDS, useValue: ['buddy brew', 'nescafe']}
     ],
     exports: [CoffeesService],
 })
